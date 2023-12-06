3af76c3ad468b277c1f8f9d0251ebefdc596f08d
cuson311 <cuson108@gmail.com>
2023-12-06 22:42:23 +0700

    fix fe

--- Controller/Statistic/StatisticController.php
+++ Controller/Statistic/StatisticController.php
@@ -27,6 +27,97 @@
     <meta charset="UTF-8">
     <title>List of Tables</title>
     <style>
+         body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        .sidebar {
+            background-color: #333;
+            color: white;
+            width: 250px;
+            height: 100vh;
+            padding: 20px;
+            position: fixed;
+            top: 0;
+            left: 0;
+            overflow-y: auto;
+        }
+
+        .sidebar a {
+            text-decoration: none;
+            color: white;
+            display: block;
+            margin-bottom: 20px;
+        }
+
+        .sidebar h2 {
+            margin-bottom: 20px;
+        }
+
+        .table-list {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+        }
+
+        .table-list li {
+            margin-bottom: 10px;
+        }
+
+        .viewcontent {
+            margin-left: 24px;
+            padding: 20px;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            background-color: white;
+        }
+
+        table th, table td {
+            padding: 10px;
+            border-bottom: 1px solid #ccc;
+            text-align: center;
+        }
+
+        table th {
+            background-color: #333;
+            color: white;
+        }
+
+        h2 {
+            margin-top: 0;
+        }
+
+        .back-button {
+            display: inline-block;
+            padding: 10px 20px;
+            margin-bottom: 20px;
+            background-color: #333;
+            color: white;
+            text-decoration: none;
+        }
+
+        .back-button:hover {
+            background-color: #555;
+        }
+        .dashboard-link {
+            display: block;
+            font-size: 1.5rem;
+            font-weight: bold;
+            text-decoration: none;
+            color: white;
+            padding: 10px 20px;
+            margin-bottom: 20px;
+            background-color: #333;
+            border-radius: 4px;
+            text-align: center;
+            transition: background-color 0.3s ease;
+        }
     </style>
 </head>
 <body>
