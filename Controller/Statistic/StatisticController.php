c0cce8df97829b653e4abd75bb5302bef5804574
cuson311 <cuson108@gmail.com>
2023-12-06 15:27:33 +0700

    add sponsor and top_player

--- Controller/Statistic/StatisticController.php
+++ Controller/Statistic/StatisticController.php
@@ -1,23 +1,70 @@
 <?php
-    if (isset($_GET['action'])){
-        $action = $_GET['action'];
-    }
-    else {
-        $action = '';
-    }
+    // if (isset($_GET['action'])){
+    //     $action = $_GET['action'];
+    // }
+    // else {
+    //     $action = '';
+    // }
 
-    switch ($action){
-        case 'topplay':{
-            require_once('View/Statistic/Top_player.php');
-            break;
-        }
-        case 'sponsor':{
-            require_once('View/Statistic/sponsor.php');
-            break;
-        }
-        default:{
-            require_once('View/SeasonResult/information.php');
-            break;
-        }
-    }
+    // switch ($action){
+    //     case 'topplay':{
+    //         require_once('View/Statistic/Top_player.php');
+    //         break;
+    //     }
+    //     case 'sponsor':{
+    //         require_once('View/Statistic/sponsor.php');
+    //         break;
+    //     }
+    //     default:{
+    //         require_once('View/SeasonResult/information.php');
+    //         break;
+    //     }
+    // }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>List of Tables</title>
+    <style>
+        /* Add your styles here */
+    </style>
+</head>
+<body>
+    <div class="sidebar">
+        <a href="http://localhost:3000/index.php" class="" style="font-size: 2rem; color: white;" onclick="redirectToDashboard()">Dashboard</a>
+        <h2 style="color: white;">List of Tables</h2>
+        <ul class="table-list">
+            <li><a href="?controller=statistic&option=show_table&table=top_player">top_player</a></li>
+            <li><a href="?controller=statistic&option=show_table&table=sponsor">sponsor</a></li>
+        </ul>
+    </div>
+    <div class="viewcontent" style="">
+        <?php
+        if (isset($_GET['table'])) {
+            $table = $_GET['table'];
+            switch ($table) {
+                case 'top_player':
+                    require_once('View/Statistic/Top_player.php');
+                    break;
+                case 'sponsor':
+                    require_once('View/Statistic/sponsor.php');
+                    break;
+                default:
+                    echo "<h2>Invalid table selection</h2>";
+                    break;
+            }
+        } else {
+            echo "<h2>Select a table from the sidebar to view its content.</h2>";
+        }
+        ?>
+    </div>
+
+    <script>
+        function redirectToDashboard() {
+            console.log("back")
+            window.location.replace('http://localhost:3000/index.php');
+        }
+    </script>
+</body>
+</html>
--- View/Statistic/sponsor.php
+++ View/Statistic/sponsor.php
@@ -1,11 +1,95 @@
+<?php
+// Database connection details
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db2.0";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Include the getPlayerTotalGoals function
+function getSponsorTotalDonations($conn) {
+    $sponsorTotalDonations = array();
+
+    // Use a SELECT query to get sponsor names
+    $getSponsorNamesQuery = "SELECT SpsName FROM sponsor";
+    $sponsorNamesResult = $conn->query($getSponsorNamesQuery);
+
+    if ($sponsorNamesResult) {
+        $sponsorNames = $sponsorNamesResult->fetch_all(MYSQLI_ASSOC);
+
+        // Loop through each sponsor
+        foreach ($sponsorNames as $sponsor) {
+            $sponsorName = $sponsor['SpsName'];
+
+            // Use a SELECT query to get the total donation for the sponsor
+            $getDonationQuery = "SELECT SUM(Finance) AS TotalDonation FROM sposor_for WHERE SF_Name = '$sponsorName'";
+            $result = $conn->query($getDonationQuery);
+
+            if ($result) {
+                $row = $result->fetch_assoc();
+                $totalDonation = ($row['TotalDonation'] !== null) ? $row['TotalDonation'] : 0.00;
+
+                // Add the sponsor and total donation to the result array
+                $sponsorTotalDonations[] = array(
+                    'SponsorName' => $sponsorName,
+                    'TotalDonation' => $totalDonation
+                );
+            } else {
+                echo "Error executing query: " . $conn->error;
+            }
+        }
+
+        return $sponsorTotalDonations;
+    } else {
+        echo "Error executing query: " . $conn->error;
+        return $sponsorTotalDonations; // Return an empty array or handle the error accordingly
+    }
+}
+
+// Call the getPlayerTotalGoals function
+$sponsorTotalDonations = getSponsorTotalDonations($conn);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nhà tài trợ</title>
+    <title>Player</title>
 </head>
 <body>
-    <p>Hiển thị nhà tài trợ</p>
+    <?php
+    if ($sponsorTotalDonations) {
+        echo "Sponsor Total Donation:\n";
+        echo "<table border='1'>";
+        echo "<tr><th>Sponsor</th><th>Total Donation</th></tr>";
+    
+        foreach ($sponsorTotalDonations as $sponsor) {
+            $sponsorName = $sponsor['SponsorName'];
+            $totalDonation = $sponsor['TotalDonation'];
+    
+            echo "<tr>";
+            echo "<td style='text-align: center;'>$sponsorName</td>";
+            echo "<td style='text-align: center;'>$totalDonation</td>";
+            echo "</tr>";
+        }
+    
+        echo "</table>";
+    } else {
+        echo "Error retrieving sponsor information.";
+    }
+?>    
+
+    <?php
+    // Close the database connection
+    $conn->close();
+    ?>
 </body>
 </html>
