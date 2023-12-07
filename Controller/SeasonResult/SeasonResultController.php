f3e83d0357cf446957fc9d783d372a7a2b9ff5b6
SaaghanSin <96587554+SaaghanSin@users.noreply.github.com>
2023-12-07 09:21:50 +0700

    commit proceduce

--- Controller/SeasonResult/SeasonResultController.php
+++ Controller/SeasonResult/SeasonResultController.php
@@ -1,19 +1,142 @@
-<?php
-    if (isset($_GET['action'])){
-        $action = $_GET['action'];
-    }
-    else {
-        $action = '';
-    }
+<!DOCTYPE html>
+<html lang="en">
 
-    switch ($action){
-        case 'rank':{
-            require_once('View/SeasonResult/ranking_chart.php');
-            break;
+<head>
+    <meta charset="UTF-8">
+    <title>List of Tables</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
         }
-        default:{
-            require_once('View/SeasonResult/information.php');
-            break;
+
+        .sidebar {
+            background-color: #111;
+            color: white;
+            width: 250px;
+            height: 100vh;
+            padding: 20px;
+            position: fixed;
+            top: 0;
+            left: 0;
+            overflow-y: auto;
         }
-    }
-?>
+
+        .sidebar a {
+            text-decoration: none;
+            color: white;
+            display: block;
+            margin-bottom: 20px;
+        }
+
+        .sidebar h2 {
+            margin-bottom: 20px;
+        }
+
+        .table-list {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+        }
+
+        .table-list li {
+            margin-bottom: 10px;
+        }
+
+        .viewcontent {
+            margin-left: 24px;
+            padding: 20px;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            background-color: white;
+        }
+
+        table th,
+        table td {
+            padding: 10px;
+            border-bottom: 1px solid #ccc;
+            text-align: center;
+        }
+
+        table th {
+            background-color: #333;
+            color: white;
+        }
+
+        h2 {
+            margin-top: 0;
+        }
+
+        .back-button {
+            display: inline-block;
+            padding: 10px 20px;
+            margin-bottom: 20px;
+            background-color: #333;
+            color: white;
+            text-decoration: none;
+        }
+
+        .back-button:hover {
+            background-color: #555;
+        }
+
+        .dashboard-link {
+            display: block;
+            font-size: 2rem;
+            font-weight: bold;
+            text-decoration: none;
+            color: white;
+            padding: 10px 20px;
+            margin-bottom: 20px;
+            /* background-color: #333; */
+            border-radius: 4px;
+            text-align: center;
+            transition: background-color 0.3s ease;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="sidebar">
+        <a href="http://localhost:3000/index.php" class="" style="font-size: 2rem; color: white;" onclick="redirectToDashboard()">Dashboard</a>
+        <h2 style="color: white;">List of Tables</h2>
+        <ul class="table-list">
+            <li><a href="?controller=season&option=show_table&table=information">Season Information</a></li>
+            <li><a href="?controller=season&option=show_table&table=ranking">Ranking Chart</a></li>
+        </ul>
+    </div>
+    <div class="viewcontent" style="">
+        <?php
+        if (isset($_GET['table'])) {
+            $table = $_GET['table'];
+            switch ($table) {
+                case 'information':
+                    require_once('View/SeasonResult/information.php');
+                    break;
+                case 'ranking':
+                    require_once('View/SeasonResult/ranking_chart.php');
+                    break;
+                default:
+                    echo "<h2>Invalid table selection</h2>";
+                    break;
+            }
+        } else {
+            echo "<h2>Select a table from the sidebar to view its content.</h2>";
+        }
+        ?>
+    </div>
+
+    <script>
+        function redirectToDashboard() {
+            console.log("back")
+            window.location.replace('http://localhost:3000/index.php');
+        }
+    </script>
+</body>
+
+</html>
--- View/SeasonResult/information.php
+++ View/SeasonResult/information.php
@@ -1,11 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Information tournament</title>
 </head>
+
 <body>
-    <p>Hiện thị toàn bộ thông tin của các đội tuyển trong mùa</p>
+    <?php
+    // Database connection details
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "db2.0";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname, 3308);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Include the GetTeamStatsInYear stored procedure
+    $getTeamStatsQuery = "CALL GetTeamStatsInYear(2022)";
+    $teamStatsResult = $conn->query($getTeamStatsQuery);
+
+    echo "Team Stats in 2022:\n";
+
+    // Display team stats in an HTML table
+    echo "<table border='1'>";
+    echo "<tr><th>Team Name</th><th>Total Red Cards</th><th>Total Yellow Cards</th><th>Total Goals</th><th>Total Assists</th></tr>";
+
+    while ($row = $teamStatsResult->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td style='text-align: center;'>" . $row['TeamName'] . "</td>";
+        echo "<td style='text-align: center;'>" . $row['TotalRedCards'] . "</td>";
+        echo "<td style='text-align: center;'>" . $row['TotalYellowCards'] . "</td>";
+        echo "<td style='text-align: center;'>" . $row['TotalGoals'] . "</td>";
+        echo "<td style='text-align: center;'>" . $row['TotalAssists'] . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+    ?>
+
+    <?php
+    // Close the database connection
+    $conn->close();
+    ?>
 </body>
+
 </html>
